a10dfce527129cb41bbff6c663f2f04d47227700
Hoang Quoc Bao <141985120+BaoHoangWork@users.noreply.github.com>
2024-12-18 23:33:06 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,6 +37,6 @@ if prompt := st.chat_input("What can I help you today?"):
     # Stream the response to the chat using `st.write_stream`, then store it in 
     # session state.
     with st.chat_message("assistant"):
-        response = st.write_stream(stream.json())
+        response = st.write_stream(stream)
         st.write(response)
     st.session_state.messages.append({"role": "assistant", "content": response})
