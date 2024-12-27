599bde847d72b4613403c02d6f6136ecb84f5e31
Hoang Quoc Bao <141985120+BaoHoangWork@users.noreply.github.com>
2024-12-27 19:59:41 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,21 +62,21 @@ with tab2:
         
     # Create a session state variable to store the chat messages. This ensures that the
     # messages persist across reruns.
-    if "messages" not in st.session_state:
-        st.session_state.messages = []
+    if "messages1" not in st.session_state:
+        st.session_state.messages1 = []
     
     # Display the existing chat messages via `st.chat_message`.
-    for message in st.session_state.messages:
-        with st.chat_message(message["role"]):
-            st.markdown(message["content"])
+    for message1 in st.session_state.messages1:
+        with st.chat_message(message1["role"]):
+            st.markdown(message1["content"])
     
     # Create a chat input field to allow the user to enter a message. This will display
     # automatically at the bottom of the page.
     if prompt := st.chat_input("What can I help you today?"):
     
         # Store and display the current prompt.
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
+        st.session_state.messages1.append({"role": "user", "content": prompt})
+        with st.chat_message1("user"):
             st.markdown(prompt)
     
         url = 'https://ragoooon.onrender.com/stream_complete'
@@ -93,4 +93,4 @@ with tab2:
         
         with st.chat_message("assistant"):
             response = st.write_stream(stream_data)
-        st.session_state.messages.append({"role": "assistant", "content": response})
+        st.session_state.messages1.append({"role": "assistant", "content": response})
