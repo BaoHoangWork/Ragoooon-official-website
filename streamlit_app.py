6006b5eb7e5de97ab2c381ee66b679a4b0b13d1f
Hoang Quoc Bao <141985120+BaoHoangWork@users.noreply.github.com>
2025-01-03 22:30:50 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,32 +26,38 @@ with tab1:
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
-    
+
+    input_container = st.empty()
+
+    with input_container:
     # Create a chat input field to allow the user to enter a message. This will display
     # automatically at the bottom of the page.
-    if prompt := st.chat_input("What can I help you today?", key=1):
+        if prompt := st.chat_input("What can I help you today?", key=1):
+        
+            # Store and display the current prompt.
+            st.session_state.messages.append({"role": "user", "content": prompt})
+            with st.chat_message("user"):
+                st.markdown(prompt)
+        
+            url = 'https://ragoooon.onrender.com/stream_complete'
+            myobj = {"prompt": prompt,"history": []}
+            stream = requests.post(url, json = myobj)
+            
+            # Stream the response to the chat using `st.write_stream`, then store it in 
+            # session state.
+            
+            def stream_data():
+                for word in stream.json()['stream']:
+                    yield word
+                    time.sleep(0.02)
+            
+            with st.chat_message("assistant"):
+                response = st.write_stream(stream_data)
+            st.session_state.messages.append({"role": "assistant", "content": response})
+            
+    # Keep the input field at the bottom of the page.
+    input_container.markdown(" ")  
     
-        # Store and display the current prompt.
-        st.session_state.messages.append({"role": "user", "content": prompt})
-        with st.chat_message("user"):
-            st.markdown(prompt)
-    
-        url = 'https://ragoooon.onrender.com/stream_complete'
-        myobj = {"prompt": prompt,"history": []}
-        stream = requests.post(url, json = myobj)
-        
-        # Stream the response to the chat using `st.write_stream`, then store it in 
-        # session state.
-        
-        def stream_data():
-            for word in stream.json()['stream']:
-                yield word
-                time.sleep(0.02)
-        
-        with st.chat_message("assistant"):
-            response = st.write_stream(stream_data)
-        st.session_state.messages.append({"role": "assistant", "content": response})
-
 # TESTING AI TAB
 with tab2:
     # Show title and description.
