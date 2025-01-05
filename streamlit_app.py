1b9641d2c2b1ac6e904ed1b89cb0a2e10091a025
Hoang Quoc Bao <141985120+BaoHoangWork@users.noreply.github.com>
2025-01-06 00:48:32 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ st.title("💬 Ragooon")
 float_init(theme=True, include_unstable_primary=False)
 
 # tab setups
-tab1, tab2 = st.tabs(["Snowflake Mistral AI /stream_complete", "/stream_chat"])
+tab1, tab2 = st.tabs(["Snowflake Mistral AI /stream_complete", "/stream_chat", "/rag_stream_complete"])
 
 # SNOWFLAKE MISTRAL TAB
 with tab1:
@@ -84,6 +84,52 @@ with tab2:
         
     # Create a session state variable to store the chat messages. This ensures that the
     # messages persist across reruns.
+    if "messages1" not in st.session_state:
+        st.session_state.messages1 = []
+    
+    # Display the existing chat messages via `st.chat_message`.
+    for message1 in st.session_state.messages2:
+        with st.chat_message(message1["role"]):
+            st.markdown(message1["content"])
+
+    with st.container():
+        prompt1 = st.chat_input(placeholder='What can I help you today?', key=2) 
+        button_b_pos = "1rem"
+        button_css = float_css_helper(width="2.2rem", bottom=button_b_pos, transition=0)
+        float_parent(css=button_css)
+        
+    if prompt1:
+        # Store and display the current prompt.
+        st.session_state.messages1.append({"role": "user", "content": prompt1})
+        with st.chat_message("user"):
+            st.markdown(prompt1)
+    
+        url = 'https://ragoooon.onrender.com/stream_chat'
+        myobj = {"prompt": prompt1, "history": st.session_state.messages1}
+        stream = requests.post(url, json = myobj)
+        
+        # Stream the response to the chat using `st.write_stream`, then store it in 
+        # session state.
+        
+        def stream_data():
+            for word in stream.json()['stream']:
+                yield word
+                time.sleep(0.02)
+        
+        with st.chat_message("assistant"):
+            response = st.write_stream(stream_data)
+        st.session_state.messages1.append({"role": "assistant", "content": response})
+
+# /rag_stream_complete
+with tab3:
+    # Show title and description.
+    st.title("💬 /rag_stream_complete API")
+    st.write(_(
+        "Latest model with database and history updated"
+    ))
+        
+    # Create a session state variable to store the chat messages. This ensures that the
+    # messages persist across reruns.
     if "messages2" not in st.session_state:
         st.session_state.messages2 = []
     
@@ -104,7 +150,7 @@ with tab2:
         with st.chat_message("user"):
             st.markdown(prompt2)
     
-        url = 'https://ragoooon.onrender.com/stream_chat'
+        url = 'https://ragoooon.onrender.com/rag_stream_complete'
         myobj = {"prompt": prompt2, "history": st.session_state.messages2}
         stream = requests.post(url, json = myobj)
         
